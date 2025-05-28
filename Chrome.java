805836ead28816d81c1c99d17a0cd787ca0cc85e
Indirapriyadharsini <70121928+Indirapriyadharsini@users.noreply.github.com>
2025-05-29 00:11:13 +0530

    Add files via upload
    Week 1 Day 1 assignment

---
+++ Chrome.java
@@ -0,0 +1,19 @@
+package org.selenium.automation;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+	
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int year = 2008;
+		char shortCutKey='C';
+		System.out.println("Version: " +version);
+		System.out.println("Developer: " +developer);
+		System.out.println("isBeta: " +isBeta);
+		System.out.println("Year: " +year);
+		System.out.println("shortCutKey: " +shortCutKey);
+	}
+
+}
